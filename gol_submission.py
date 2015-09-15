23b2a224e236c3b8eb098ac88ba391e8a89cba3b
Stephanie Lockwood-Childs <sjl@vctlabs.com>
2015-09-15 13:04:29 -0700

    Partially implement Gem / Biggie lists
    URL, Banner, Screenshot are getting picked out properly, but
    the text part needs to be picked out and split into Text1 (first paragraph,
    pre-screenshot) and Text2 (other paragraphs, post-screenshot).
    
    Probably I should be removing URL, Banner, and Screenshot as they
    are found, leaving just the text part to split into Text1 and Text2...

--- gol_submission.py
+++ gol_submission.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-import os, sys, re
+import os, sys, re, itertools
 import wikitextparser
 import jinja2
 
-## compatibility hack: teach python2 to open files with an encoding
+## compatibility hacks: teach python2 to open files with an encoding
 if sys.version_info[0] < 3:
     import codecs
     open = codecs.open
@@ -36,6 +36,25 @@ def read_wiki_data(input_filename):
 
     return article_sections
 
+def pairs(input_list):
+    "s -> (s0,s1), (s2,s3), (s4, s5), ..."
+    evens = itertools.islice(input_list, 0, len(input_list), 2)
+    odds = itertools.islice(input_list, 1, len(input_list), 2)
+    return zip(evens, odds)
+
+def wiki_to_bbcode(text):
+    # translate bold to bbcode
+    text = re.sub(r"'''([^']+)'''",r"[b]\1[\\b]", text)
+
+    # translate italics to bbcode
+    text = re.sub(r"''([^']+)''",r"[i]\1[\\i]", text)
+
+    # translate urls to bbcode
+    text = re.sub(r"\[(http[^ ]+) ([^]]+)\]", r"[url=\1]\2[\\url]", text)
+
+    return text
+
+
 def reformat_wiki_data(input_filename):
     # pick out major sections
     article_sections = read_wiki_data(input_filename)
@@ -47,22 +66,70 @@ def reformat_wiki_data(input_filename):
         # strip off section title
         text = re.sub(r"^=+[^=]+=+\n","", text)
 
-        # translate bold to bbcode
-        text = re.sub(r"'''([^']+)'''",r"[b]\1[\\b]", text)
-
-        # translate italics to bbcode
-        text = re.sub(r"''([^']+)''",r"[i]\1[\\i]", text)
-
-        # translate urls to bbcode
-        text = re.sub(r"\[(http[^ ]+) ([^]]+)\]", r"[url=\1]\2[\\url]", text)
-
-        reformatted_sections[section] = text
+        reformatted_sections[section] = wiki_to_bbcode(text)
 
 
     # translate gem and biggie sections into lists
-    #@@@TODO
-    reformatted_sections["gem_list"] = []
-    reformatted_sections["biggie_list"] = []
+    #for list_type in ["gem", "biggie"]:
+    for list_type in ["gem"]:
+        list_name = list_type + "_list" # e.g. gem_list 
+        section_name = list_type + "_paragraphs" # e.g. gem_paragraphs
+        paragraphs = article_sections[section_name]
+        reformatted_sections[list_name] = []
+
+        # strip off main section title
+        paragraphs = re.sub(r"^=+[^=]+=+\n+","", paragraphs)
+        #DBG#print(paragraphs.encode('utf-8'))
+
+        # split into subsection for each project 
+        projects = re.split(r"(===[^=]+===)", paragraphs)
+        projects = filter(None, projects) # get rid of empty strings after split()
+        for (proj_header, proj_body) in pairs(projects):
+            entry = {}
+
+            # pick out url from project header
+            
+            # "=== [http://some/url this is the name] ==="
+            # grab http://some/url
+            try:
+                url = re.search(r"\[(http\S+)", proj_header).group(1)
+            except:
+                url = "INVALID_URL"
+                print("Failed to find url in header:\n%s" % proj_header.encode('utf-8'))
+
+            # pick out banner, screenshot, text1, text2 from the project body
+            
+            # "Banner http://banner/url blah blah"
+            # grab http://banner/url
+            try:
+                banner = re.search(r"banner (http\S+)", proj_body, re.IGNORECASE).group(1)
+            except:
+                banner = "INVALID_BANNER"
+                print("Failed to find banner in body:\n%s" % proj_body.encode('utf-8'))
+
+            # "Screenshot http://screenshot/url blah blah"
+            # grab http://screenshot/url
+            try:
+                screenshot = re.search(r"screenshot (http\S+)", proj_body, re.IGNORECASE).group(1)
+            except:
+                screenshot = "INVALID_SCREENSHOT"
+                print("Failed to find screenshot in body:\n%s" % proj_body.encode('utf-8'))
+
+            #@@@ TODO
+            text1 = "PRE-SCREENSHOT PARAGRAPH"
+            text2 = "POST-SCREENSHOT PARAGRAPH(S)"
+
+            entry = { "url": url,
+                      "banner": banner,
+                      "screenshot": screenshot,
+                      "text1": wiki_to_bbcode(text1),
+                      "text2": wiki_to_bbcode(text2) }
+
+            #DBG#print("HEADER:", proj_header.encode('utf-8'))
+            #DBG#print("BODY:", proj_body.encode('utf-8'))
+            #DBG#print("ENTRY:", entry.encode('utf-8'))
+
+            reformatted_sections[list_name].append(entry)
 
     return reformatted_sections
 
